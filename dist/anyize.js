5535f60e967cfeb0acdfd31a1a65b02b0b072837
Jesús Espino <jesus.espino@kaleidos.net>
2014-03-19 09:47:27 +0100

    More robust generic default values

--- dist/anyize.js
+++ dist/anyize.js
@@ -3,25 +3,38 @@
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   Anyize = (function() {
+    Anyize.prototype._randomString = function(length) {
+      var chars, x;
+      if (length == null) {
+        length = 5;
+      }
+      chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split('');
+      return ((function() {
+        var _i, _results;
+        _results = [];
+        for (x = _i = 1; 1 <= length ? _i <= length : _i >= length; x = 1 <= length ? ++_i : --_i) {
+          _results.push(chars[Math.floor(Math.random() * chars.length)]);
+        }
+        return _results;
+      })()).join('');
+    };
+
     function Anyize(options) {
+      this.crossScreenAnimation = __bind(this.crossScreenAnimation, this);
       this.defaultAnimation = __bind(this.defaultAnimation, this);
       var anyizeAudioMarkup, anyizeImageMarkup;
+      this._randomId = this._randomString(5);
       this.imgUrl = options.imgUrl || null;
       this.mp3Url = options.mp3Url || null;
       this.oggUrl = options.oggUrl || null;
-      this.initialCss = options.initialCss || {
-        "position": "fixed",
-        "bottom": "-250px",
-        "right": "0",
-        "display": "block"
-      };
       this.animation = options.animation || this.defaultAnimation;
+      this.reset = options.reset || this.defaultReset;
       if (this.imgUrl) {
-        anyizeImageMarkup = "<img id='anyize-image' style='display: none' src='" + this.imgUrl + "' />";
+        anyizeImageMarkup = "<img id='" + this._randomId + "-image' style='display: none' src='" + this.imgUrl + "' />";
         $('body').append(anyizeImageMarkup);
       }
       if (this.mp3Url || this.oggUrl) {
-        anyizeAudioMarkup = "<audio id='anyize-audio' preload='auto'>";
+        anyizeAudioMarkup = "<audio id='" + this._randomId + "-audio' preload='auto'>";
         if (this.mp3Url) {
           anyizeAudioMarkup += "<source src='" + this.mp3Url + "' />";
         }
@@ -31,29 +44,73 @@
         anyizeAudioMarkup += "</audio>";
         $('body').append(anyizeAudioMarkup);
       }
-      $('#anyize-image').css(this.initialCss);
+      $("#" + this._randomId + "-image").on('load', (function(_this) {
+        return function() {
+          console.log($("#" + _this._randomId + "-image").height() - 10);
+          _this.initialCss = options.initialCss || {
+            "position": "fixed",
+            "bottom": -$("#" + _this._randomId + "-image").height() - 10,
+            "right": "0",
+            "display": "block"
+          };
+          return $("#" + _this._randomId + "-image").css(_this.initialCss);
+        };
+      })(this));
     }
 
-    Anyize.prototype.reset = function() {
-      return $('#anyize-image').css(this.initialCss);
+    Anyize.prototype.defaultReset = function(imgElement, audioElement) {
+      return imgElement.css(this.initialCss);
     };
 
     Anyize.prototype.fire = function() {
-      return this.animation();
+      return this.animation($("#" + this._randomId + "-image"), $("#" + this._randomId + "-audio"));
     };
 
-    Anyize.prototype.defaultAnimation = function() {
-      $('#anyize-audio').each(function(idx, elem) {
+    Anyize.prototype.defaultAnimation = function(imgElement, audioElement) {
+      audioElement.each(function(idx, elem) {
         return elem.play();
       });
-      $('#anyize-image').animate({
+      return imgElement.animate({
         "bottom": "0"
-      }, 750, function() {
-        return $(this).delay(1500).animate({
-          "bottom": "-250px"
-        }, 750);
+      }, 750, (function(_this) {
+        return function() {
+          return imgElement.delay(1500).animate({
+            "bottom": -imgElement.height() - 10
+          }, 750, function() {
+            return _this.reset(imgElement, audioElement);
+          });
+        };
+      })(this));
+    };
+
+    Anyize.prototype.crossScreenAnimation = function(imgElement, audioElement) {
+      audioElement.each(function(idx, elem) {
+        return elem.play();
       });
-      return this.reset;
+      return imgElement.animate({
+        "right": $(window).width()
+      }, 5000, (function(_this) {
+        return function() {
+          return _this.reset(imgElement, audioElement);
+        };
+      })(this));
+    };
+
+    Anyize.prototype.setCrossScreenCss = function() {
+      var setCss;
+      setCss = (function(_this) {
+        return function() {
+          _this.initialCss = {
+            "position": "fixed",
+            "bottom": "0",
+            "right": -$("#" + _this._randomId + "-image").height() - 10,
+            "display": "block"
+          };
+          return $("#" + _this._randomId + "-image").css(_this.initialCss);
+        };
+      })(this);
+      setCss();
+      return $("#" + this._randomId + "-image").on("load", setCss);
     };
 
     return Anyize;
--- src/anyize.coffee
+++ src/anyize.coffee
@@ -1,24 +1,23 @@
 class Anyize
+    _randomString: (length=5) ->
+        chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split('')
+        return (chars[Math.floor(Math.random() * chars.length)] for x in [1..length]).join('')
+
     constructor: (options) ->
+        @_randomId = @_randomString(5)
         @imgUrl = options.imgUrl or null
         @mp3Url = options.mp3Url or null
         @oggUrl = options.oggUrl or null
 
-        @initialCss = options.initialCss or {
-            "position":"fixed",
-            "bottom": "-250px",
-            "right" : "0",
-            "display" : "block"
-        }
-
         @animation = options.animation or @defaultAnimation
+        @reset = options.reset or @defaultReset
 
         if @imgUrl
-            anyizeImageMarkup = "<img id='anyize-image' style='display: none' src='#{@imgUrl}' />"
+            anyizeImageMarkup = "<img id='#{@_randomId}-image' style='display: none' src='#{@imgUrl}' />"
             $('body').append(anyizeImageMarkup)
 
         if @mp3Url or @oggUrl
-            anyizeAudioMarkup = "<audio id='anyize-audio' preload='auto'>"
+            anyizeAudioMarkup = "<audio id='#{@_randomId}-audio' preload='auto'>"
             if @mp3Url
                 anyizeAudioMarkup += "<source src='#{@mp3Url}' />"
             if @oggUrl
@@ -26,18 +25,43 @@ class Anyize
             anyizeAudioMarkup += "</audio>"
             $('body').append(anyizeAudioMarkup)
 
-        $('#anyize-image').css(@initialCss)
+        $("##{@_randomId}-image").on 'load', =>
+            @initialCss = options.initialCss or {
+                "position":"fixed"
+                "bottom": -$("##{@_randomId}-image").height() - 10
+                "right" : "0"
+                "display" : "block"
+            }
+            $("##{@_randomId}-image").css(@initialCss)
 
-    reset: ->
-        $('#anyize-image').css(@initialCss)
+    defaultReset: (imgElement, audioElement)->
+        imgElement.css(@initialCss)
 
     fire: ->
-        @animation()
+        @animation($("##{@_randomId}-image"), $("##{@_randomId}-audio"))
 
-    defaultAnimation: =>
-        $('#anyize-audio').each (idx, elem) -> elem.play()
-        $('#anyize-image').animate { "bottom" : "0" }, 750, ->
-            $(@).delay(1500).animate { "bottom" : "-250px" }, 750
-        @reset
+    defaultAnimation: (imgElement, audioElement) =>
+        audioElement.each (idx, elem) -> elem.play()
+        imgElement.animate { "bottom" : "0" }, 750, =>
+            imgElement.delay(1500).animate { "bottom" : -imgElement.height() - 10 }, 750, =>
+                @reset(imgElement, audioElement)
+
+    crossScreenAnimation: (imgElement, audioElement) =>
+        audioElement.each (idx, elem) -> elem.play()
+        imgElement.animate { "right" : $(window).width() }, 5000, =>
+            @reset(imgElement, audioElement)
+
+    setCrossScreenCss: ->
+        setCss = =>
+            @initialCss = {
+                "position":"fixed"
+                "bottom": "0"
+                "right": -$("##{@_randomId}-image").height() - 10
+                "display" : "block"
+            }
+            $("##{@_randomId}-image").css(@initialCss)
+
+        setCss()
+        $("##{@_randomId}-image").on "load", setCss
 
 @Anyize = Anyize
